8bbf7772bc6ee998796ecbfb2d9872df6dc5eb88
Alexankharin <42934906+Alexankharin@users.noreply.github.com>
2023-09-10 15:33:53 +0200

    new sckimage versions

--- logger.py
+++ logger.py
@@ -4,31 +4,40 @@ import torch.nn.functional as F
 import imageio
 
 import os
-from skimage.draw import circle
+from skimage.draw import disk as circle
 import pdb
 import matplotlib.pyplot as plt
 import collections
 
-class Logger:
-    def __init__(self, log_dir, checkpoint_freq=100, visualizer_params=None, zfill_num=8, log_file_name='log.txt'):
 
+class Logger:
+    def __init__(
+        self,
+        log_dir,
+        checkpoint_freq=100,
+        visualizer_params=None,
+        zfill_num=8,
+        log_file_name="log.txt",
+    ):
         self.loss_list = []
         self.cpk_dir = log_dir
-        self.visualizations_dir = os.path.join(log_dir, 'train-vis')
+        self.visualizations_dir = os.path.join(log_dir, "train-vis")
         if not os.path.exists(self.visualizations_dir):
             os.makedirs(self.visualizations_dir)
-        self.log_file = open(os.path.join(log_dir, log_file_name), 'a')
+        self.log_file = open(os.path.join(log_dir, log_file_name), "a")
         self.zfill_num = zfill_num
         self.visualizer = Visualizer(**visualizer_params)
         self.checkpoint_freq = checkpoint_freq
         self.epoch = 0
-        self.best_loss = float('inf')
+        self.best_loss = float("inf")
         self.names = None
 
     def log_scores(self, loss_names):
         loss_mean = np.array(self.loss_list).mean(axis=0)
 
-        loss_string = "; ".join(["%s - %.5f" % (name, value) for name, value in zip(loss_names, loss_mean)])
+        loss_string = "; ".join(
+            ["%s - %.5f" % (name, value) for name, value in zip(loss_names, loss_mean)]
+        )
         loss_string = str(self.epoch).zfill(self.zfill_num) + ") " + loss_string
 
         print(loss_string, file=self.log_file)
@@ -36,86 +45,139 @@ class Logger:
         self.log_file.flush()
 
     def visualize_rec(self, inp, out):
-        image = self.visualizer.visualize(inp['driving'], inp['source'], out)
-        imageio.imsave(os.path.join(self.visualizations_dir, "%s-rec.png" % str(self.epoch).zfill(self.zfill_num)), image)
+        image = self.visualizer.visualize(inp["driving"], inp["source"], out)
+        imageio.imsave(
+            os.path.join(
+                self.visualizations_dir,
+                "%s-rec.png" % str(self.epoch).zfill(self.zfill_num),
+            ),
+            image,
+        )
 
     def save_cpk(self, emergent=False):
         cpk = {k: v.state_dict() for k, v in self.models.items()}
-        cpk['epoch'] = self.epoch
-        cpk_path = os.path.join(self.cpk_dir, 'final-checkpoint.pth.tar') 
+        cpk["epoch"] = self.epoch
+        cpk_path = os.path.join(self.cpk_dir, "final-checkpoint.pth.tar")
         if not (os.path.exists(cpk_path) and emergent):
             torch.save(cpk, cpk_path)
 
     @staticmethod
-    def load_cpk(checkpoint_path, generator=None, discriminator=None, kp_detector=None,
-                 optimizer_generator=None, optimizer_discriminator=None, optimizer_kp_detector=None,avd_network=None):
+    def load_cpk(
+        checkpoint_path,
+        generator=None,
+        discriminator=None,
+        kp_detector=None,
+        optimizer_generator=None,
+        optimizer_discriminator=None,
+        optimizer_kp_detector=None,
+        avd_network=None,
+    ):
         num_gpu = torch.cuda.device_count()
         if num_gpu == 1:
-            checkpoint = torch.load(checkpoint_path,map_location='cuda:0')
+            checkpoint = torch.load(checkpoint_path, map_location="cuda:0")
         else:
-            checkpoint = torch.load(checkpoint_path,map_location='cpu')
+            checkpoint = torch.load(checkpoint_path, map_location="cpu")
         if generator is not None:
-            ckp_generator = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['generator'].items())
+            ckp_generator = collections.OrderedDict(
+                (k.replace(".module.", "."), v)
+                for k, v in checkpoint["generator"].items()
+            )
             generator.load_state_dict(ckp_generator)
         if kp_detector is not None:
-            ckp_kp_detector = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['kp_detector'].items())
+            ckp_kp_detector = collections.OrderedDict(
+                (k.replace(".module.", "."), v)
+                for k, v in checkpoint["kp_detector"].items()
+            )
             kp_detector.load_state_dict(ckp_kp_detector)
         if avd_network is not None:
-            if 'avd_network' in checkpoint:
-                avd_network.load_state_dict(checkpoint['avd_network'])
+            if "avd_network" in checkpoint:
+                avd_network.load_state_dict(checkpoint["avd_network"])
                 # ckp_avd_network = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['avd_network'].items())
                 # avd_network.load_state_dict(ckp_avd_network)
         if discriminator is not None:
             try:
-                ckp_discriminator = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['discriminator'].items())
+                ckp_discriminator = collections.OrderedDict(
+                    (k.replace(".module.", "."), v)
+                    for k, v in checkpoint["discriminator"].items()
+                )
                 discriminator.load_state_dict(ckp_discriminator)
             except:
-               print ('No discriminator in the state-dict. Dicriminator will be randomly initialized')
+                print(
+                    "No discriminator in the state-dict. Dicriminator will be randomly initialized"
+                )
         if optimizer_generator is not None:
-            ckp_optimizer_generator = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['optimizer_generator'].items())
+            ckp_optimizer_generator = collections.OrderedDict(
+                (k.replace(".module.", "."), v)
+                for k, v in checkpoint["optimizer_generator"].items()
+            )
             optimizer_generator.load_state_dict(ckp_optimizer_generator)
         if optimizer_discriminator is not None:
             try:
-                ckp_optimizer_discriminator = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['optimizer_discriminator'].items())
+                ckp_optimizer_discriminator = collections.OrderedDict(
+                    (k.replace(".module.", "."), v)
+                    for k, v in checkpoint["optimizer_discriminator"].items()
+                )
                 optimizer_discriminator.load_state_dict(ckp_optimizer_discriminator)
             except RuntimeError as e:
-                print ('No discriminator optimizer in the state-dict. Optimizer will be not initialized')
+                print(
+                    "No discriminator optimizer in the state-dict. Optimizer will be not initialized"
+                )
         if optimizer_kp_detector is not None:
-            ckp_optimizer_kp_detector = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['optimizer_kp_detector'].items())
+            ckp_optimizer_kp_detector = collections.OrderedDict(
+                (k.replace(".module.", "."), v)
+                for k, v in checkpoint["optimizer_kp_detector"].items()
+            )
             optimizer_kp_detector.load_state_dict(ckp_optimizer_kp_detector)
 
-        return checkpoint['epoch']
+        return checkpoint["epoch"]
 
     @staticmethod
-    def load_cpk_bg(checkpoint_path, generator=None, kp_detector=None, bg_predictor=None,
-                 optimizer=None):
-                #  checkpoint, generator, kp_detector, bg_predictor, None,optimizer,
+    def load_cpk_bg(
+        checkpoint_path,
+        generator=None,
+        kp_detector=None,
+        bg_predictor=None,
+        optimizer=None,
+    ):
+        #  checkpoint, generator, kp_detector, bg_predictor, None,optimizer,
         num_gpu = torch.cuda.device_count()
         if num_gpu == 1:
-            checkpoint = torch.load(checkpoint_path,map_location='cuda:0')
+            checkpoint = torch.load(checkpoint_path, map_location="cuda:0")
         else:
-            checkpoint = torch.load(checkpoint_path,map_location='cpu')
+            checkpoint = torch.load(checkpoint_path, map_location="cpu")
         if generator is not None:
-            ckp_generator = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['generator'].items())
+            ckp_generator = collections.OrderedDict(
+                (k.replace(".module.", "."), v)
+                for k, v in checkpoint["generator"].items()
+            )
             generator.load_state_dict(ckp_generator)
         if kp_detector is not None:
-            ckp_kp_detector = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['kp_detector'].items())
+            ckp_kp_detector = collections.OrderedDict(
+                (k.replace(".module.", "."), v)
+                for k, v in checkpoint["kp_detector"].items()
+            )
             kp_detector.load_state_dict(ckp_kp_detector)
         if bg_predictor is not None:
-            ckp_bg_predictor = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['bg_predictor'].items())
+            ckp_bg_predictor = collections.OrderedDict(
+                (k.replace(".module.", "."), v)
+                for k, v in checkpoint["bg_predictor"].items()
+            )
             bg_predictor.load_state_dict(ckp_bg_predictor)
-        
+
         if optimizer is not None:
-            ckp_optimizer_generator = collections.OrderedDict((k.replace('.module.','.'),v) for k,v in checkpoint['optimizer'].items())
+            ckp_optimizer_generator = collections.OrderedDict(
+                (k.replace(".module.", "."), v)
+                for k, v in checkpoint["optimizer"].items()
+            )
             optimizer.load_state_dict(ckp_optimizer_generator)
-       
-        return checkpoint['epoch']
+
+        return checkpoint["epoch"]
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if 'models' in self.__dict__:
+        if "models" in self.__dict__:
             self.save_cpk()
         self.log_file.close()
 
@@ -135,7 +197,7 @@ class Logger:
 
 
 class Visualizer:
-    def __init__(self, kp_size=5, draw_border=False, colormap='gist_rainbow'):
+    def __init__(self, kp_size=5, draw_border=False, colormap="gist_rainbow"):
         self.kp_size = kp_size
         self.draw_border = draw_border
         self.colormap = plt.get_cmap(colormap)
@@ -151,7 +213,9 @@ class Visualizer:
         return image
 
     def create_image_column_with_kp(self, images, kp):
-        image_array = np.array([self.draw_image_with_kp(v, k) for v, k in zip(images, kp)])
+        image_array = np.array(
+            [self.draw_image_with_kp(v, k) for v, k in zip(images, kp)]
+        )
         return self.create_image_column(image_array)
 
     def create_image_column(self, images):
@@ -175,58 +239,59 @@ class Visualizer:
 
         # Source image with keypoints
         source = source.data.cpu()
-        kp_source = out['kp_source']['value'].data.cpu().numpy()
+        kp_source = out["kp_source"]["value"].data.cpu().numpy()
         source = np.transpose(source, [0, 2, 3, 1])
         images.append((source, kp_source))
 
         # Equivariance visualization
-        if 'transformed_frame' in out:
-            transformed = out['transformed_frame'].data.cpu().numpy()
+        if "transformed_frame" in out:
+            transformed = out["transformed_frame"].data.cpu().numpy()
             transformed = np.transpose(transformed, [0, 2, 3, 1])
-            transformed_kp = out['transformed_kp']['value'].data.cpu().numpy()
+            transformed_kp = out["transformed_kp"]["value"].data.cpu().numpy()
             images.append((transformed, transformed_kp))
 
         # Driving image with keypoints
-        kp_driving = out['kp_driving']['value'].data.cpu().numpy()
+        kp_driving = out["kp_driving"]["value"].data.cpu().numpy()
         driving = driving.data.cpu().numpy()
         driving = np.transpose(driving, [0, 2, 3, 1])
         images.append((driving, kp_driving))
 
         # Deformed image
-        if 'deformed' in out:
-            deformed = out['deformed'].data.cpu().numpy()
+        if "deformed" in out:
+            deformed = out["deformed"].data.cpu().numpy()
             deformed = np.transpose(deformed, [0, 2, 3, 1])
             images.append(deformed)
 
         # Result with and without keypoints
-        prediction = out['prediction'].data.cpu().numpy()
+        prediction = out["prediction"].data.cpu().numpy()
         prediction = np.transpose(prediction, [0, 2, 3, 1])
-        if 'kp_norm' in out:
-            kp_norm = out['kp_norm']['value'].data.cpu().numpy()
+        if "kp_norm" in out:
+            kp_norm = out["kp_norm"]["value"].data.cpu().numpy()
             images.append((prediction, kp_norm))
         images.append(prediction)
 
-
         ## Occlusion map
-        if 'occlusion_map' in out:
-            occlusion_map = out['occlusion_map'].data.cpu().repeat(1, 3, 1, 1)
+        if "occlusion_map" in out:
+            occlusion_map = out["occlusion_map"].data.cpu().repeat(1, 3, 1, 1)
             occlusion_map = F.interpolate(occlusion_map, size=source.shape[1:3]).numpy()
             occlusion_map = np.transpose(occlusion_map, [0, 2, 3, 1])
             images.append(occlusion_map)
 
         # Deformed images according to each individual transform
-        if 'sparse_deformed' in out:
+        if "sparse_deformed" in out:
             full_mask = []
-            for i in range(out['sparse_deformed'].shape[1]):
-                image = out['sparse_deformed'][:, i].data.cpu()
+            for i in range(out["sparse_deformed"].shape[1]):
+                image = out["sparse_deformed"][:, i].data.cpu()
                 image = F.interpolate(image, size=source.shape[1:3])
-                mask = out['mask'][:, i:(i+1)].data.cpu().repeat(1, 3, 1, 1)
+                mask = out["mask"][:, i : (i + 1)].data.cpu().repeat(1, 3, 1, 1)
                 mask = F.interpolate(mask, size=source.shape[1:3])
                 image = np.transpose(image.numpy(), (0, 2, 3, 1))
                 mask = np.transpose(mask.numpy(), (0, 2, 3, 1))
 
                 if i != 0:
-                    color = np.array(self.colormap((i - 1) / (out['sparse_deformed'].shape[1] - 1)))[:3]
+                    color = np.array(
+                        self.colormap((i - 1) / (out["sparse_deformed"].shape[1] - 1))
+                    )[:3]
                 else:
                     color = np.array((0, 0, 0))
 
